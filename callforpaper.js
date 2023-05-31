608739ad4addb0c27bae0011001a25e774c4a41b
chirag r_s <chiragrs2003@gmail.com>
2023-05-31 18:22:36 +0530

    mobile-size

--- callforpaper.js
+++ callforpaper.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function () {
   window.addEventListener("scroll", function () {
-    if (window.pageYOffset > 0) {
+    if (window.pageYOffset > -1) {
       document.querySelector(".header").classList.add("active");
     } else {
       document.querySelector(".header").classList.remove("active");
